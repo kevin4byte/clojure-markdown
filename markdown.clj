3852060ae8bfe813a6e8dade0fa106356b3f7856
xiaowl <xiaowl@mobijia.com>
2012-07-15 21:19:00 +0800

    Methods to process inline elements, <strong><em>.

--- markdown.clj
+++ markdown.clj
@@ -21,3 +21,38 @@
           head (drop-trailing-chars (drop lh line) "\n #")]
       (format "<h%s>%s</h%s>" lh (apply str head) lh))))
 
+(defn
+  process-close-italic
+  [content]
+  (loop [init [] [fst & more] content]
+    (case fst
+      \* (if 
+        (and 
+          (not= \* (first more)) ; next char is not *.
+                                 ; *aa**bb**cc* should be translate to <em>aa<strong>bb</strong>cc</em>
+          (not= \space (last init))); previous char is not space, *a * is not a valid input.
+           [true init] (recur (conj init fst) more))
+      (if (nil? fst) [false init] (recur (conj init fst) more)))))
+
+(defn
+  process-bold
+  []
+  ())
+
+(defn
+  process-italic
+  [content]
+  (let [[c & more] content]
+    (case c
+      \* (process-bold more)
+      \space content ;list item should be hanndled at start of processing a line
+      (process-close-italic more))))
+
+(defn
+  process-inline
+  [content]
+  (loop [fst (first content) remain (rest content)]
+    (case fst
+      \* (2)
+      \_ (3)
+      (4))))
