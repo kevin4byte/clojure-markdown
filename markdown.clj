d1a8617b9b4ff55f92360894765c1a40536fb711
xiaowl <xiaowl@mobijia.com>
2012-07-15 17:43:12 +0800

    Add function 'drop-trailing-chars' to trim trailing chars

--- markdown.clj
+++ markdown.clj
@@ -1,16 +1,4 @@
-(defn
-	process-inline
-	([line] 
-		(loop [c (first line) prev nil other (rest line)]
-			(case c
-				\# (println (str "Enter heading, " (= prev \newline)))
-				\* (println "strong")
-				(println (str "normal char, " c)))
-			(if (not (nil? (first other))) (recur (first other) c (rest other))))))
-
-(defn trip-car)
-
-
+;; This is a personal practice project, to drive me to learn clojure. ;-)
 (defn
   count-leading-hash
   [line]
@@ -18,15 +6,18 @@
     (if (> cnt 6) 6 cnt)))
 
 (defn
+  drop-trailing-chars
+  [s chars]
+  (loop [end (last s) ln (drop-last s)]
+    (if (> (.indexOf chars (str end)) -1)
+      (recur (last ln) (drop-last ln))
+      (concat ln `(~end)))))
+
+(defn
   ^{:doc "Process <h[1-6]> elements"}
-  process-head
+  process-heading
   ([line]
-    (let [lh (count-leading-hash line)]
-    (str "<h" lh ">" (apply str (reverse
-     (drop-while
-      #(= % \#) 
-      (drop 1
-        (reverse 
-          (drop-while
-           #(= % \#) 
-           line)))))) "</h" lh ">"))))
+    (let [lh (count-leading-hash line)
+          head (drop-trailing-chars (drop lh line) "\n #")]
+      (format "<h%s>%s</h%s>" lh (apply str head) lh))))
+
