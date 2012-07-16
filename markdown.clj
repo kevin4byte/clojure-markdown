534b13d9bb99a1e47d1c8972c095ba4b8da9aa11
xiaowl <kevin.xiaowl@gmail.com>
2012-07-16 23:47:30 +0800

    Add 'group-bold' function, to find out all bold groups.

--- markdown.clj
+++ markdown.clj
@@ -2,16 +2,13 @@
 (defn
   count-leading-hash
   [line]
-  (let [cnt (count (take-while #(= % \#) line))]
-    (if (> cnt 6) 6 cnt)))
+  (count (take-while #(= % \#) (take 6 line))))
 
 (defn
   drop-trailing-chars
   [s chars]
-  (loop [end (last s) ln (drop-last s)]
-    (if (> (.indexOf chars (str end)) -1)
-      (recur (last ln) (drop-last ln))
-      (concat ln `(~end)))))
+  (loop [line s]
+    (if (some #(= % (last line)) chars) (recur (butlast line)) line)))
 
 (defn
   ^{:doc "Process <h[1-6]> elements"}
@@ -22,37 +19,16 @@
       (format "<h%s>%s</h%s>" lh (apply str head) lh))))
 
 (defn
-  process-close-italic
-  [content]
-  (loop [init [] [fst & more] content]
-    (case fst
-      \* (if 
-        (and 
-          (not= \* (first more)) ; next char is not *.
-                                 ; *aa**bb**cc* should be translate to <em>aa<strong>bb</strong>cc</em>
-          (not= \space (last init))); previous char is not space, *a * is not a valid input.
-           [true init] (recur (conj init fst) more))
-      (if (nil? fst) [false init] (recur (conj init fst) more)))))
-
-(defn
-  process-bold
-  []
-  ())
-
-(defn
-  process-italic
-  [content]
-  (let [[c & more] content]
+  group-bold
+  [line]
+  (loop [idx 0 [c & m] line prev [nil nil] groups []]
     (case c
-      \* (process-bold more)
-      \space content ;list item should be hanndled at start of processing a line
-      (process-close-italic more))))
-
-(defn
-  process-inline
-  [content]
-  (loop [fst (first content) remain (rest content)]
-    (case fst
-      \* (2)
-      \_ (3)
-      (4))))
+        \* (if (even? (count groups)); even indexes?
+              (if (and (= \* (first m)) (not= \space (first (rest m))));
+                (recur (+ 2 idx) (rest m) (conj (vec (rest prev)) (first m)) (conj groups idx))
+                (recur (inc idx) m (conj (vec (rest prev)) c) groups))
+              (if (and (= \* (last prev)) (not= \space (first prev)) (not= \* (first m)))
+                (recur (inc idx) m (conj (vec (rest prev)) c) (conj groups idx))
+                (recur (inc idx) m (conj (vec (rest prev)) c) groups)))
+        nil (if (even? (count groups)) groups (vec (butlast groups)))
+        (recur (inc idx) m (conj (vec (rest prev)) c) groups))))
