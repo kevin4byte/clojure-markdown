3f45aba8cb178412a0986daaabccf462fad522db
xiaowl <kevin.xiaowl@gmail.com>
2012-07-19 01:03:41 +0800

    Add method to parse *em* and *strong*

--- markdown.clj
+++ markdown.clj
@@ -31,7 +31,7 @@
         close-bold? #(and (= (take 2 %) '(\* \*)) (not= (first (nthrest % 2)) \*))]
     (cond
       (nil? head) nil
-      (not= \space head) (if (close-bold? more) [(inc idx) (nthrest more 2)] (recur (inc idx) more))
+      (not= \space head) (if (close-bold? more) [(+ 2 idx) (nthrest more 2)] (recur (inc idx) more))
       :else (recur (inc idx) more))))
 
 (defn- find-bold
@@ -50,7 +50,7 @@
       :else (let [[group more :as bold] (find-bold offset line)]
         (if (nil? bold) 
           groups
-          (recur (conj groups group) more (+ 2 (last group))))))))
+          (recur (conj groups group) more (inc (last group))))))))
 ;;
 ;; find-bolds and group-bolds are functional indentical. which is better?
 ;;
@@ -104,3 +104,107 @@
         (if (nil? em)
           groups
           (recur (conj groups group) (+ 1 (last group)) more))))))
+
+(defn- find-open-link
+  [line idx]
+  (let [[head & more] line]
+    (cond
+      (nil? head) nil
+      (= \[ head) [idx more]
+      :else (recur more (inc idx)))))
+
+(defn- find-close-link-1
+  [line idx]
+  (let [[head & more] line]
+    (cond
+      (nil? head) nil
+      (= \] head) (if (= \( (first more)) [idx more] (recur more (inc idx)))
+      :else (recur more (inc idx)))))
+
+(defn- find-close-link-2
+  [line idx]
+  (let [[head & more] line]
+    (cond
+      (nil? head) nil
+      (= \) head) [idx more]
+      :else (recur more (inc idx)))))
+
+(defn- find-close-link
+  [line]
+  (if-let [[pos1 more] (find-close-link-1 line 0)]
+    (if-let [[pos2 more] (find-close-link-2 more 0)]
+      [[pos1 (+ 1 pos1 pos2)] more])))
+
+(defn- extract-content
+  [line rage]
+  (let [[s e] rage]
+    (subs line (inc s) e)))
+
+(defn- find-link
+  [line offset]
+  (if-let [[link-start more] (find-open-link line 0)]
+    (if-let [[[link-end content-end] more] (find-close-link line)]
+      [(vec (map #(+ offset %) [link-start link-end (inc link-end) content-end])) more])))
+
+(defn find-links
+  [line]
+  (loop [ line line
+          offset 0
+          groups []]
+    (cond
+      (empty? line) groups
+      :else (let [[group more :as link] (find-link line offset)]
+        (if (nil? link) groups
+          (recur more (inc (last group)) (conj groups group)))))))
+
+(defn- parse-bold
+  [line]
+  (format "<strong>%s</strong>" (apply str (nthrest (drop-last 2 line) 2))))
+
+(defn- rreplace
+  [line replfn group]
+  (let [[open close] group
+        body (replfn (drop open (take (inc close) line)))
+        tail (apply str (drop (inc close) line))]
+        (str body tail)))
+
+(defn parse-bolds
+  [line]
+  (let [groups (set (flatten (find-bolds line)))]
+    (if (empty? groups)
+      line
+      (loop [[head & more] line
+             pos 0
+             open? false
+             result []]
+        (if (nil? head) 
+          (apply str result)
+          (cond
+            (groups pos) (if open?
+                            (recur more (inc pos) false (conj (pop result) "</strong>"))
+                            (recur (rest more) (+ 2 pos) true (conj result "<strong>")))
+            :else (recur more (inc pos) open? (conj result head))))))))
+
+(defn parse-ems
+  [line]
+  (let [groups (set (flatten (find-ems line)))]
+    (if (empty? groups)
+      line
+      (loop [[head & more] line
+             pos 0
+             open? false
+             result []]
+        (if (nil? head) 
+          (apply str result)
+          (cond
+            (groups pos) (if open?
+                            (recur more (inc pos) false (conj result "</em>"))
+                            (recur more (inc pos) true (conj result "<em>")))
+            :else (recur more (inc pos) open? (conj result head))))))))
+
+(defn parse-phrase-emphasis
+  [line]
+  (let [bolded (parse-bolds line)];bold has high priority
+    (parse-ems bolded)))
+
+
